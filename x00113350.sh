c0980f50532ef50e753c66dbe9bd49483617f977
x00113350 <x00113350@gmail.com>
2014-10-14 16:11:07 +0100

    60 60 Pass Expected 60 Pass
    60 30
    Component Fail
    Expected 48 Fail
    
    60 50
    Fail
    Expected 56 Pass
    
    30 50
    Component Fail
    Expected 38 Component Fail
    
    10 100
    Component Fail
    Expected 46 Fail
    
    00 30
    Component Fail
    Expected 72 Pass

---
+++ x00113350.sh
@@ -0,0 +1,25 @@
+echo 
+60 60
+Pass
+Expected 60 Pass
+
+60 30
+Component Fail
+Expected 48 Fail
+
+60 50
+Fail
+Expected 56 Pass
+
+30 50
+Component Fail
+Expected 38 Component Fail
+
+10 100
+Component Fail
+Expected 46 Fail
+
+00 30
+Component Fail
+Expected 72 Pass
+
